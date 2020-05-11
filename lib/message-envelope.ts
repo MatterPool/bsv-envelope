4e3b6a30b2126a6b3476c73aa958ec4704d9aa70
Attila <bitcoinfiles@protonmail.com>
2020-05-11 00:17:28 -0600

    Adding metanet now

--- lib/message-envelope.ts
+++ lib/message-envelope.ts
@@ -1,11 +1,15 @@
 import * as bsv from 'bsv';
 import * as protobuf from 'protobufjs';
+const messagesDefn = require('../messages.protobuf.bundle.json');
+const root = protobuf.Root.fromJSON(messagesDefn);
+import * as Long from 'long';
 
 export class MessageEnvelope {
     private payloadIdentifier_;
     private payloadType_;
     private payload_;
-    private envelopeVersion_ = '00';
+    static envelopeProtocolIdentifier = 'bd';
+    static envelopeVersion = '00';
     private encryptedPayloads_: Array<{
         sender: string,
         receivers: Array<{
@@ -21,8 +25,16 @@ export class MessageEnvelope {
     } = undefined;
     private constructor(
         private payloadProtocol_ = 'F',
-        private payloadVersion_ = '00',
+        private payloadVersion_ = 0,
     ) {
+    }
+
+    setPayloadVersion(v) {
+        this.payloadVersion_ = v;
+    }
+
+    getPayloadVersion() {
+        return this.payloadVersion_;
     }
 
     setPayloadIdentifier(s: string) {
@@ -49,66 +61,172 @@ export class MessageEnvelope {
         return this.payload_;
     }
 
-    async toScript(): bsv.Script {
+    toScript(): bsv.Script {
         const out = new bsv.Script();
         out.add(bsv.Opcode.OP_FALSE);
         out.add(bsv.Opcode.OP_RETURN);
-        // Envelope Protocol Version
-        out.add(Buffer.from(this.envelopeVersion_, 'hex'));
-
+        // Protocol and Envelope Protocol Version
+        out.add(Buffer.from(MessageEnvelope.envelopeProtocolIdentifier + MessageEnvelope.envelopeVersion, 'hex'));
         if (!this.payloadProtocol_ || !this.payloadProtocol_.length) {
             throw new Error('invalid payload protocol');
         }
-
-        out.add(Buffer.from(this.payloadProtocol_, 'hex'));
+        out.add(Buffer.from(this.payloadProtocol_, 'utf8'));
         let message;
-        return await protobuf.load("messages.proto")
-        .then((root) => {
-            const Envelope = root.lookupType("protobuf.Envelope");
-            message = Envelope.create({
-                Version: this.payloadVersion_,
-                Type: this.payloadType_,
-                Identifier: this.payloadIdentifier_,
-                // EncryptedPayloads: // added below
-            }); // or use .fromObject if conversion is necessary
+        const Envelope = root.lookupType("protobuf.Envelope");
+        message = Envelope.create({
+            Version: this.payloadVersion_,
+            Type: Buffer.from(this.payloadType_, 'utf8'),
+            Identifier: Buffer.from(this.payloadIdentifier_, 'utf8') // this.payloadIdentifier_,
+            // EncryptedPayloads: // added below
+        }); // or use .fromObject if conversion is necessary
 
-            if (this.metanet_) {
-                message.MetaNet = {
-                    Index: this.metanet_.index,
-                    Parent: this.metanet_.parent
-                };
-            }
-            const EncryptedPayload = root.lookupType("protobuf.EncryptedPayload");
-            const Receiver = root.lookupType("protobuf.Receiver");
-            const protoBufEnvelopeEncryptedPayloads: any = [];
-            for (const encryptedPayload of this.encryptedPayloads_) {
-                const protobufReceivers: any = [];
-                for (const receiver of encryptedPayload.receivers) {
-                    const protobufReceiver = Receiver.create({
-                        Index: receiver.index,
-                        EncryptedKey: receiver.encryptedKey
-                    });
-                    protobufReceivers.push(protobufReceiver);
-                }
-                const protobufEncryptedPayload = EncryptedPayload.create({
-                    Sender: encryptedPayload.sender,
-                    Receivers: protobufReceivers,
-                    Payload: encryptedPayload.payload,
+        if (this.metanet_) {
+            message.MetaNet = {
+                Index: this.metanet_.index,
+                Parent: this.metanet_.parent
+            };
+        }
+        const EncryptedPayload = root.lookupType("protobuf.EncryptedPayload");
+        const Receiver = root.lookupType("protobuf.Receiver");
+        const protoBufEnvelopeEncryptedPayloads: any = [];
+        for (const encryptedPayload of this.encryptedPayloads_) {
+            const protobufReceivers: any = [];
+            for (const receiver of encryptedPayload.receivers) {
+                const protobufReceiver = Receiver.create({
+                    Index: receiver.index,
+                    EncryptedKey: receiver.encryptedKey
                 });
-
-                protoBufEnvelopeEncryptedPayloads.push(protobufEncryptedPayload);
+                protobufReceivers.push(protobufReceiver);
             }
-            message.EncryptedPayloads = protoBufEnvelopeEncryptedPayloads;
-            out.add(Envelope.encode(message).finish());
+            const protobufEncryptedPayload = EncryptedPayload.create({
+                Sender: encryptedPayload.sender,
+                Receivers: protobufReceivers,
+                Payload: encryptedPayload.payload,
+            });
 
-            if (this.payload_) {
-                out.add(this.payload_);
-            }
-            return out;
-        });
+            protoBufEnvelopeEncryptedPayloads.push(protobufEncryptedPayload);
+        }
+        message.EncryptedPayloads = protoBufEnvelopeEncryptedPayloads;
+        const encodedProtobufMessage = Envelope.encode(message).finish();
+        out.add(encodedProtobufMessage);
+
+        //console.log('encodedProtobufMessage', encodedProtobufMessage);
+        // const d = Envelope.decode(encodedProtobufMessage);
+        // console.log('dddd', d);
+
+        if (this.payload_) {
+            out.add(this.payload_);
+        }
+        return out;
     }
 
-    static fromScript(script: string): MessageEnvelope {
-        return new MessageEnvelope();
+    static fromScript(scriptStr: bsv.Script): MessageEnvelope {
+        const Envelope = root.lookupType("protobuf.Envelope");
+        let script = new bsv.Script(scriptStr);
+        const message = new MessageEnvelope();
+
+        console.log('script', script.chunks);
+        if (script.chunks[0].opcodenum !== bsv.Opcode.OP_FALSE) {
+            throw new Error('Invalid script: OP_FALSE not found');
+        }
+        if (script.chunks[1].opcodenum !== bsv.Opcode.OP_RETURN) {
+            throw new Error('Invalid script: OP_RETURN not found');
+        }
+        if (script.chunks[2].buf.toString('hex') !== (MessageEnvelope.envelopeProtocolIdentifier + MessageEnvelope.envelopeVersion)) {
+            throw new Error('Invalid script: Version not found');
+        }
+        if (script.chunks[3].buf.toString('utf8') !== 'F') {
+            throw new Error('Invalid script: File system F not found');
+        }
+
+        const envelope = Envelope.decode(script.chunks[4].buf);
+        message.setPayloadVersion(Long.fromValue(envelope['Version']).toNumber());
+        message.setPayloadType(envelope['Type'].toString('utf8'));
+        message.setPayloadIdentifier(envelope['Identifier'].toString('utf8'));
+
+        if (script.chunks[5] && script.chunks[5].buf) {
+            message.setPayload(script.chunks[5].buf);
+        }
+
+        /*
+
+
+// Deserialize reads the Message from an OP_RETURN script.
+func Deserialize(buf *bytes.Reader) (*Message, error) {
+	var result Message
+
+	// Protocol ID
+	var opCode byte
+	var err error
+	opCode, result.payloadProtocol, err = bitcoin.ParsePushDataScript(buf)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to parse protocol ID")
+	}
+	if len(result.payloadProtocol) == 0 && opCode != bitcoin.OP_FALSE { // Non push data op code
+		return nil, ErrNotEnvelope
+	}
+
+	// Envelope
+	_, envelopeData, err := bitcoin.ParsePushDataScript(buf)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to read MetaNet data")
+	}
+
+	var envelope protobuf.Envelope
+	if len(envelopeData) != 0 {
+		if err = proto.Unmarshal(envelopeData, &envelope); err != nil {
+			return nil, errors.Wrap(err, "Failed envelope protobuf unmarshaling")
+		}
+	}
+
+	result.payloadVersion = envelope.GetVersion()
+	result.payloadType = envelope.GetType()
+	result.payloadIdentifier = envelope.GetIdentifier()
+
+	// MetaNet
+	pbMetaNet := envelope.GetMetaNet()
+	if pbMetaNet != nil {
+		result.metaNet = &MetaNet{
+			index:  pbMetaNet.GetIndex(),
+			parent: pbMetaNet.GetParent(),
+		}
+	}
+
+	// Encrypted payloads
+	pbEncryptedPayloads := envelope.GetEncryptedPayloads()
+	result.encryptedPayloads = make([]*EncryptedPayload, 0, len(pbEncryptedPayloads))
+	for _, pbEncryptedPayload := range pbEncryptedPayloads {
+		var encryptedPayload EncryptedPayload
+
+		// Sender
+		encryptedPayload.sender = pbEncryptedPayload.GetSender()
+
+		// Receivers
+		pbReceivers := pbEncryptedPayload.GetReceivers()
+		encryptedPayload.receivers = make([]*Receiver, 0, len(pbReceivers))
+		for _, pbReceiver := range pbReceivers {
+			encryptedPayload.receivers = append(encryptedPayload.receivers, &Receiver{
+				index:        pbReceiver.GetIndex(),
+				encryptedKey: pbReceiver.GetEncryptedKey(),
+			})
+		}
+
+		// Payload
+		encryptedPayload.payload = pbEncryptedPayload.GetPayload()
+
+		result.encryptedPayloads = append(result.encryptedPayloads, &encryptedPayload)
+	}
+
+	// Public payload
+	_, result.payload, err = bitcoin.ParsePushDataScript(buf)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to read payload")
+	}
+
+	return &result, nil
+}
+*/
+        console.log('message, message)', message);
+        return message;
     }
 }
--- test/envelope.test.js
+++ test/envelope.test.js
@@ -15,17 +15,76 @@ describe('message-envelope', () => {
       const data = Buffer.from('hello world2', 'utf8');
       message.setPayload(data);
       expect(message.getPayload().toString('hex')).to.eql(data.toString('hex'));
+      expect(message).to.eql({
+         "encryptedPayloads_": [],
+         "metanet_": undefined,
+         "payloadIdentifier_": "company_logo.jpg",
+         "payloadProtocol_": "F",
+         "payloadType_": "image/jpg",
+         "payloadVersion_": 0,
+         "payload_": Buffer.from([
+            104,
+            101,
+            108,
+            108,
+            111,
+            32,
+            119,
+            111,
+            114,
+            108,
+            100,
+            50,
+         ])
+      });
 
-      const outputScript = await message.toScript();
-      expect(outputScript.toASM()).to.eql('0 OP_RETURN 00 080012068a66a07bf8e91a0b7289a96a7cbf9688288e98 68656c6c6f20776f726c6432');
-      expect(index.Envelope.fromScript(outputScript)).to.eql(message);
+      const outputScript = message.toScript();
+      expect(outputScript.toASM()).to.eql('0 OP_RETURN bd00 46 08001209696d6167652f6a70671a10636f6d70616e795f6c6f676f2e6a7067 68656c6c6f20776f726c6432');
+
+      const fromScriptMessage = index.Envelope.fromScript(outputScript);
+      expect(fromScriptMessage).to.eql(message);
 
    });
 
    it('should be valid minimal envelope with metanet node', async () => {
-     /*
+      const message = new index.Envelope();
+      message.setPayloadIdentifier('company_logo.jpg');
+      expect(message.getPayloadIdentifier()).to.eql('company_logo.jpg');
 
-     */
+      message.setPayloadType('image/jpg');
+      expect(message.getPayloadType()).to.eql('image/jpg');
+
+      const data = Buffer.from('hello world2', 'utf8');
+      message.setPayload(data);
+      expect(message.getPayload().toString('hex')).to.eql(data.toString('hex'));
+      expect(message).to.eql({
+         "encryptedPayloads_": [],
+         "metanet_": undefined,
+         "payloadIdentifier_": "company_logo.jpg",
+         "payloadProtocol_": "F",
+         "payloadType_": "image/jpg",
+         "payloadVersion_": 0,
+         "payload_": Buffer.from([
+            104,
+            101,
+            108,
+            108,
+            111,
+            32,
+            119,
+            111,
+            114,
+            108,
+            100,
+            50,
+         ])
+      });
+
+      const outputScript = message.toScript();
+      expect(outputScript.toASM()).to.eql('0 OP_RETURN bd00 46 08001209696d6167652f6a70671a10636f6d70616e795f6c6f676f2e6a7067 68656c6c6f20776f726c6432');
+
+      const fromScriptMessage = index.Envelope.fromScript(outputScript);
+      expect(fromScriptMessage).to.eql(message);
 
    });
 
